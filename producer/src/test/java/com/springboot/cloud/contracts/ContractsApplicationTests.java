ccc890cb01f3fc398476a724c1de18d707ef50a5
Olga <olga.diaz-gutierrez@vontobel.com>
2021-03-12 20:17:20 +0100

    initial setup

---
+++ producer/src/test/java/com/springboot/cloud/contracts/ContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cloud.contracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
